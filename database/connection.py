0ebcd4398c844a0d4527d27993d429d3103ea43d
Aisha2mohammed <hayunadus111@gmail.com>
2025-04-30 12:29:12 +0300

    Add a new developemental files

---
+++ database/connection.py
@@ -0,0 +1,14 @@
+# database/connection.py
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# PostgreSQL DB URL
+SQLALCHEMY_DATABASE_URL = "postgresql://dispatch_user:strongpassword@localhost/bus_dispatch_db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
