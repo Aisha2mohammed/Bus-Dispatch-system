1e020b9ffb52f6ca1820870b86d6fe2251471f44
Aisha2mohammed <hayunadus111@gmail.com>
2025-04-30 12:29:44 +0300

    Add a new developemental files

---
+++ main.py
@@ -0,0 +1,13 @@
+# main.py
+
+from fastapi import FastAPI
+from modules.users.routes import router as user_router
+from database.connection import Base, engine
+
+app = FastAPI()
+
+# Create DB tables
+Base.metadata.create_all(bind=engine)
+
+# Include user routes from router
+app.include_router(user_router)
