29b194de239b197f58aa59e9cac5775a2395a445
Aisha2mohammed <hayunadus111@gmail.com>
2025-06-03 23:58:58 +0300

    Deleted some uneccessary line of  code

--- BDS-CHATGPT/modules/users/auth_routes.py
+++ BDS-CHATGPT/modules/users/auth_routes.py
@@ -7,19 +7,6 @@ from auth.jwt_handler import create_access_token
 
 router = APIRouter()
 
-# @router.post("/login")
-# def login_user(user: schemas.UserLogin, db: Session = Depends(get_db)):
-#     db_user = db.query(models.User).filter(models.User.email == user.email).first()
-#     if not db_user:
-#         raise HTTPException(status_code=404, detail="Invalid credentials")
-
-#     if not verify_password(user.password, db_user.hashed_password):
-#         raise HTTPException(status_code=401, detail="Incorrect password")
-
-#     token = create_access_token({"sub": db_user.email, "role": db_user.role})
-#     return {"access_token": token, "token_type": "bearer"}
-
-
 from pydantic import BaseModel, EmailStr
 
 class UserBase(BaseModel):
