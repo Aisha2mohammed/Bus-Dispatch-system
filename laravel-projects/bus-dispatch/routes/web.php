f6ba1b1c4fbc075140888c437ce1b42d92319843
Aisha2mohammed <hayunadus111@gmail.com>
2025-06-27 22:47:51 +0300

    Add system control

--- laravel-projects/bus-dispatch/routes/web.php
+++ laravel-projects/bus-dispatch/routes/web.php
@@ -1,11 +1,49 @@
+
+
+// use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UserController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
+// Authentication Routes
+Auth::routes(['register' => false]); // Disable registration if not needed
+
+// Admin Routes
+Route::prefix('admin')->middleware(['auth', 'admin'])->group(function () {
+    Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+    Route::resource('users', UserController::class)->names([
+        'index' => 'admin.users.index',
+        'create' => 'admin.users.create',
+        'store' => 'admin.users.store',
+        'show' => 'admin.users.show',
+        'edit' => 'admin.users.edit',
+        'update' => 'admin.users.update',
+        'destroy' => 'admin.users.destroy',
+    ]);
+});
+
+// Home Route
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Default Route
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
